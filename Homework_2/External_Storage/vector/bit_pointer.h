e5f065dd6e37c79988e4b558436e67a8399d8948
Dmitriev_Svuatoslav <emerld@mail.ru>
2017-03-28 09:55:46 +0300

    Documentation for bit_pointer class

--- Homework_2/External_Storage/vector/bit_pointer.h
+++ Homework_2/External_Storage/vector/bit_pointer.h
@@ -1,6 +1,7 @@
-//
-// Created by svuatoslav on 3/24/17.
-//
+//---------------------------------------
+//! @file bit_pointer.h
+//! Definition of the bit_pointer class that is used to return bool value
+//---------------------------------------
 
 #ifndef VECTOR_BIT_POINTER_H
 #define VECTOR_BIT_POINTER_H
@@ -14,9 +15,36 @@ class bit_pointer
     char* byte_pointer_;
     size_t index_;
 public:
+
+//---------------------------------------
+//! @brief Constructor
+//! @param byte_pointer Pointer to the byte that contains the referenced bit
+//! @param index Location of the referenced bit in the byte
+//---------------------------------------
+
     bit_pointer(char* byte_pointer, size_t index);
+
+//---------------------------------------
+//! @brief Type cast to boolean
+//! @return Copy of the referenced boolean value
+//---------------------------------------
+
     operator bool() const;
+
+//---------------------------------------
+//! @brief Assignment operator
+//! @param value A boolean value to be written to the referenced bit
+//! @return Reference to the value parameter
+//---------------------------------------
+
     const bool& operator = (const bool& value);
+
+//---------------------------------------
+//! @brief Assignment operator
+//! @param value A bit pointer, that contains a boolean value to be written to the referenced bit
+//! @return Reference to this object
+//---------------------------------------
+
     const bit_pointer& operator = (const bit_pointer& that);
 };
 
